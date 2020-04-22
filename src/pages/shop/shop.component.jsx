158bd4b89736880241aa036dd797262b01067354
inam95 <mfihassan99@gmail.com>
2020-04-22 08:33:11 +0530

    colllection-preview added

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+class ShopPage extends Component {
+  state = {
+    collection: SHOP_DATA
+  };
+
+  render() {
+    const { collection } = this.state;
+    return (
+      <div className="shop-page">
+        {collection.map(({ id, ...otherProps }) => (
+          <CollectionPreview key={id} {...otherProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
