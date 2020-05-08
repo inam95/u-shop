e22dc95e41cbce72fad601d836c33d8aed262c6a
inam95 <mfihassan99@gmail.com>
2020-05-08 08:12:45 +0530

    google sign in custom button

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, ...otherProps }) => (
-  <button className="custom-button" {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+  <button
+    className={classNames('custom-button', {
+      'google-sign-in': isGoogleSignIn
+    })}
+    {...otherProps}
+  >
     {children}
   </button>
 );
