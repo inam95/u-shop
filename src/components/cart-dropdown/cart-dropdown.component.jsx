1370b3150d8d73f09a3cddd486a65eedf3f727cc
inam95 <mfihassan99@gmail.com>
2020-07-11 19:10:59 +0530

    reselect integration

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+import { selectCartItems } from '../../redux/cart/cart.selector';
+
+import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
-import { connect } from 'react-redux';
-import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
@@ -17,8 +19,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
